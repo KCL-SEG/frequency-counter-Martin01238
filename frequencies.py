55e3a0df47aa8e8bbddaaa84dd6c8f412ab70f3b
Martin Tang <84841301+Martin01238@users.noreply.github.com>
2023-09-30 18:09:31 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    # Your code goes here=]
+    for a in items:
+        a = str(a)
+        frequencies[a] = 0
+    for b in items:
+        b = str(b)
+        frequencies[b] = frequencies[b] + 1
     return frequencies
